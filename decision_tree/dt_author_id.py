fb226dc829130dad3f806a1952783e28a4b46166
wayne <molockree@gmail.com>
2017-08-23 11:48:13 +0800

    practice

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,7 +24,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+#print len(features_train[0])
+
+clf.fit(features_train, labels_train)
+
+print clf.score(features_test, labels_test)
 
 #########################################################
 
